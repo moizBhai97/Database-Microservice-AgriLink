45d0fe537060448394388afb74c24ec83875c0b9
Harris <saif.harris@yahoo.com>
2024-11-10 23:57:10 +0500

    restored index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const app = require('./src/app');
-//const { port, dbUrl } = require('./src/config');
+const { port, dbUrl } = require('./src/config');
 const mongoose = require('mongoose');
 
-const dbUrl='mongodb+srv://harrissaif01:harris1234@cluster0.i5ngqeq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 mongoose.connect(dbUrl)
     .then(() => app.listen(3000, () => console.log('Server Running on port:', 3000)))
     .catch((error) => console.log(error.message));
