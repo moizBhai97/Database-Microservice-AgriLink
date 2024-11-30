eb70a5c4f9a65be561f26a85ee1e357b45dc2f30
Amna Shehzad <shehzadamna21@gmail.com>
2024-11-30 08:46:31 +0500

    changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,19 +1,19 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers['authorization']; 
-  const token = authHeader && authHeader.split(' ')[1];
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized: Missing token' });
-  }
+// const authMiddleware = (req, res, next) => {
+//   const authHeader = req.headers['authorization']; 
+//   const token = authHeader && authHeader.split(' ')[1];
+//   if (!token) {
+//     return res.status(401).json({ message: 'Unauthorized: Missing token' });
+//   }
 
-  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: 'Forbidden: Invalid token' });
-    }
-    req.user = decoded; 
-    next();
-  });
-};
+//   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+//     if (err) {
+//       return res.status(403).json({ message: 'Forbidden: Invalid token' });
+//     }
+//     req.user = decoded; 
+//     next();
+//   });
+// };
 
-module.exports = authMiddleware;
+// module.exports = authMiddleware;
