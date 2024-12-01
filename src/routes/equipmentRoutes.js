252bf9846ec4a2ce1b5eed0efb0fd50888750ad2
Harris <saif.harris@yahoo.com>
2024-12-01 15:13:21 +0500

    Modification of Equipement Route

--- src/routes/equipmentRoutes.js
+++ src/routes/equipmentRoutes.js
@@ -4,12 +4,12 @@ const equipmentController = require('../controllers/equipmentController');
 const router = express.Router();
 
 router.route('/')
-    .get(equipmentController.getAllEquipment)
-    .post(equipmentController.createEquipment);
+  .get(equipmentController.getAllEquipment)
+  .post(equipmentController.createEquipment);
 
 router.route('/:id')
-    .get(equipmentController.getEquipmentById)
-    .put(equipmentController.updateEquipment)
-    .delete(equipmentController.deleteEquipment);
+  .get(equipmentController.getEquipmentById)
+  .put(equipmentController.updateEquipment)
+  .delete(equipmentController.deleteEquipment);
 
 module.exports = router;
