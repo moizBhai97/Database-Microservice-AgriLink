46d5a06ca28bcabc24347d762dc99ee95ef55d73
moizBhai97 <moizrootsdha@gmail.com>
2024-12-02 19:33:55 +0500

    Changed / route

--- src/routes/index.js
+++ src/routes/index.js
@@ -34,7 +34,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.json({
-        message: 'Welcome to the Farmally API',
+        message: 'Welcome to the AgriLink DB API',
     });
 });
 
